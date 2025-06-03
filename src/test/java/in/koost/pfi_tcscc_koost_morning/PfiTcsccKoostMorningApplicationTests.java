7f33128382e50e4731b390370d111b87e0ddb4b6
kookatlas <seenugadu225@gmail.com>
2025-06-03 18:52:36 +0530

    first commit

---
+++ src/test/java/in/koost/pfi_tcscc_koost_morning/PfiTcsccKoostMorningApplicationTests.java
@@ -0,0 +1,13 @@
+package in.koost.pfi_tcscc_koost_morning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfiTcsccKoostMorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
